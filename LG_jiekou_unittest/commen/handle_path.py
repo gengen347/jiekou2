5a9aaf347c2ddf75a036fe24ff7fdcc790938ca2
ligen <xia_xie_de>
2021-12-04 19:15:34 +0800

    2

--- LG_jiekou_unittest/commen/handle_path.py
+++
@@ -1,7 +0,0 @@
-import os
-BASE_PATH=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-CONFIG_PATH=os.path.join(BASE_PATH,'conf')
-DATA_PATH=os.path.join(BASE_PATH,'data')
-CASE_PATH=os.path.join(BASE_PATH,'testcase')
-REPORT_PATH=os.path.join(BASE_PATH,'reports')
-LOG_PATH=os.path.join(BASE_PATH,'logs')
